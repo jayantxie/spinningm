6e942c9e153e08866d05f77985a4974d456193fe
xiezhengyao <xiezhengyao@bytedance.com>
2024-09-26 16:25:35 +0800

    fix: nohup

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ cd $(dirname "${BASH_SOURCE[0]}")
 
 # build and run server in proc 0-3
 cd server && go build
-nohup GOGC=1000 taskset -c 0-3 ./server &
+GOGC=1000 taskset -c 0-3 nohup ./server &
 
 # build and run client in proc 4-12
 cd ../client && go build
-nohup GOGC=1000 taskset -c 4-15 ./client -n=10000000 -con=4 &
+GOGC=1000 taskset -c 4-15 nohup ./client -n=10000000 -con=4 &
 
 SERVER_PID=`ps -ef | grep server | grep -v grep | awk '{print $2}'`
 pidstat -p $SERVER_PID 1 60
