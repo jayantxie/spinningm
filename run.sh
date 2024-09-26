4167069f9183de4ec47272bf13851f9c5db64e5e
xiezhengyao <xiezhengyao@bytedance.com>
2024-09-26 16:26:53 +0800

    rename

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 cd $(dirname "${BASH_SOURCE[0]}")
 
 # build and run server in proc 0-3
-cd server && go build
-GOGC=1000 taskset -c 0-3 nohup ./server &
+cd server && go build && mv server spiningm_server
+GOGC=1000 taskset -c 0-3 nohup ./spiningm_server &
 
 # build and run client in proc 4-12
 cd ../client && go build
 GOGC=1000 taskset -c 4-15 nohup ./client -n=10000000 -con=4 &
 
-SERVER_PID=`ps -ef | grep server | grep -v grep | awk '{print $2}'`
+SERVER_PID=`ps -ef | grep spiningm_server | grep -v grep | awk '{print $2}'`
 pidstat -p $SERVER_PID 1 60
